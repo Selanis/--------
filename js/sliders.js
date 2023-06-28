103ea0a0fa3bf4f9e507b872785fa6821ae023ac
Ekaterina Polekhova <k.polexowa@yandex.ru>
2023-06-28 23:49:12 +0300

    слайдер

--- js/sliders.js
+++ js/sliders.js
@@ -1,3 +1,13 @@
-import Swiper from 'swiper';
-import 'swiper/css';
+document.addEventListener('DOMContentLoaded', function() {
+    const showSlider = new Swiper('.brands-carousel', {
+        loop: true,
+        slidesPerView: 3,
+        speed: 1300,
+        centeredSlides: true,
+        navigation: {
+            nextEl: '.brands-carousel__next',
+            prevEl: '.brands-carousel__prev'
+        }
+    })
+})
 
